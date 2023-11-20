4fed2d840765b1f5f613d1f838c3e19f2c8dc8aa
rivten <rivten@riseup.net>
2023-11-20 18:53:35 +0100

    clickable button

--- static/index.mjs
+++ static/index.mjs
@@ -3,52 +3,19 @@ import htm from './htm.mjs';
 
 const html = htm.bind(h);
 
-class Clock extends Component {
-    state = { time: Date.now() }
-
-    componentDidMount() {
-        this.timer = setInterval(
-            () => {
-                this.setState({ time: Date.now() });
-            }, 1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
-
-    render() {
-        let time = new Date().toLocaleTimeString();
-        return html`
-            <span>${time}</span>
-        `;
-    }
-}
-
 class App extends Component {
-    state = { value: '', name: 'sailor' }
+    state = {}
 
-    onInput = ev => {
-        this.setState({ value: ev.target.value });
-    }
-
-    onSubmit = ev => {
+    onClick = ev => {
+        console.log(ev.target.getAttribute("data-pick"));
         ev.preventDefault()
-
-        this.setState({ name: this.state.value });
     }
 
     render() {
         return html`
-            <${Clock} />
-            <div>
-                <h1>Hello ${this.state.name} !</h1>
-                <form onSubmit=${this.onSubmit}>
-                    <input type="text" value=${this.state.value} onInput=${this.onInput} />
-                    <button type="submit">Update</button>
-                </form>
-            </div>
+            <h1>Hello !</h1>
+            <button type="button" onClick=${this.onClick} data-pick="white">Pick white</button>
+            <button type="button" onClick=${this.onClick} data-pick="black">Pick black</button>
         `;
     }
 }
