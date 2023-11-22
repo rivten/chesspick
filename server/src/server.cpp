354e3ce1db23a268e4aca7a9cf93c3b80820f5a2
rivten <rivten@riseup.net>
2023-11-22 22:43:10 +0100

    basic server

---
+++ server/src/server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+
+static constexpr const char* PORT = "8081";
+static constexpr int BACKLOG = 10;
+static constexpr int RECV_BUFFER_SIZE = 1024;
+
+int main() {
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (int rv = getaddrinfo(nullptr, PORT, &hints, &servinfo); rv != 0) {
+        std::cerr << "[ERROR] getaddrinfo: " << gai_strerror(rv) << '\n';
+        return 1;
+    }
+
+    struct addrinfo* p = nullptr;
+
+    int sockfd;
+    for (p = servinfo; p != nullptr; p = p->ai_next) {
+        if (sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol); sockfd == -1) {
+            continue;
+        }
+
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            std::exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if (p == nullptr) {
+        std::cerr << "[ERROR] server failed to bind\n";
+        std::exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        std::cerr << "[ERROR] server failed to listen\n";
+    }
+    
+    std::cerr << "[INFO] server waiting for connections\n";
+
+    while (true) {
+        struct sockaddr_storage their_addr;
+        socklen_t sin_size = sizeof(their_addr);
+        int fd = accept(sockfd, reinterpret_cast<struct sockaddr*>(&their_addr), &sin_size);
+        if (fd == -1) {
+            std::cerr << "[ERROR] server failed to accept\n";
+        }
+
+        char recv_buffer[RECV_BUFFER_SIZE];
+        for (int recv_count; (recv_count = recv(fd, &recv_buffer, RECV_BUFFER_SIZE, 0)) != 0;) {
+            std::cout << recv_buffer;
+            memset(&recv_buffer, 0, RECV_BUFFER_SIZE);
+        }
+    }
+
+    return 0;
+}
