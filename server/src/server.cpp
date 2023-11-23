692348ebd97cb5057fe2ae85355c5d14a5942dae
rivten <rivten@riseup.net>
2023-11-23 23:11:56 +0100

    basics of fastcgi

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -5,10 +5,64 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <cstring>
+#include <cstddef>
+#include <cassert>
+#include <unordered_map>
+#include <fstream>
+#include <vector>
 
 static constexpr const char* PORT = "8081";
 static constexpr int BACKLOG = 10;
-static constexpr int RECV_BUFFER_SIZE = 1024;
+static constexpr int RECV_BUFFER_SIZE = 65535;
+
+enum class FCGIHeaderType {
+    BEGIN_REQUEST = 1,
+    END_REQUEST = 3,
+    FCGI_PARAMS = 4,
+    FCGI_STDIN = 5,
+    FCGI_STDOUT = 6,
+    FCGI_STDERR = 7,
+};
+
+struct FCGIHeader {
+    int version;
+    FCGIHeaderType type;
+    int request_id;
+    int content_length;
+    int padding_length;
+};
+
+FCGIHeader parse_header(char buffer[], size_t buffer_index) {
+    FCGIHeader result;
+    result.version = static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 0]));
+    std::cerr << result.version << '\n';
+    assert(result.version == 1);
+    result.type = static_cast<FCGIHeaderType>(static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 1])));
+    result.request_id = static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 2])) * 256 + static_cast<int>(static_cast<unsigned  char>(buffer[buffer_index + 3]));
+    result.content_length = static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 4])) * 256 + static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 5]));
+    result.padding_length = static_cast<int>(static_cast<unsigned char>(buffer[buffer_index + 6]));
+    return result;
+}
+
+struct FCGIRecord {
+    int version;
+    int type;
+    int request_id;
+    std::string content_data;
+};
+
+std::string handle_request(std::string fcgi_stdin, const std::unordered_map<std::string, std::string>& params) {
+    std::string http_content = fcgi_stdin;
+    std::string fcgi_stdout;
+    fcgi_stdout += "HTTP/1.1 201 Created\r\n";
+    fcgi_stdout += "Content-Length: ";
+    fcgi_stdout += std::to_string(http_content.length());
+    fcgi_stdout += "\r\n";
+    fcgi_stdout += "\r\n";
+    fcgi_stdout += http_content;
+
+    return fcgi_stdout;
+}
 
 int main() {
 #if 0
@@ -72,11 +126,155 @@ int main() {
 
         char recv_buffer[RECV_BUFFER_SIZE];
         for (int recv_count; (recv_count = recv(fd, &recv_buffer, RECV_BUFFER_SIZE, 0)) != 0;) {
+            if (recv_count < 0) {
+                continue;
+            }
             std::cerr << "[VERBOSE] data received\n";
             std::cerr << recv_count << '\n';
-            for (int i = 0; i < recv_count; ++i) {
-                std::cerr << std::hex << static_cast<int>(recv_buffer[i]);
+            size_t buffer_index = 0;
+            std::unordered_map<std::string, std::string> params;
+            std::string fcgi_stdin;
+            int request_id = 0;
+
+#if 1
+            std::ofstream raw_request ("raw.bin", std::ios::out | std::ios::binary);
+            raw_request.write(recv_buffer, recv_count);
+            raw_request.close();
+#endif
+            while (buffer_index < recv_count) {
+                std::cerr << "[VERBOSE] buffer index: " << buffer_index << '\n';
+                FCGIHeader header = parse_header(recv_buffer, buffer_index);
+                if (request_id == 0) {
+                    request_id = header.request_id;
+                } else {
+                    assert(request_id == header.request_id);
+                }
+                buffer_index += 8; // size of the fcgi header
+                std::cerr << "[VERBOSE] header: \n"
+                    << "[VERBOSE]      version: " << header.version << '\n'
+                    << "[VERBOSE]      type: " << static_cast<int>(header.type) << '\n'
+                    << "[VERBOSE]      request_id: " << header.request_id << '\n'
+                    << "[VERBOSE]      content_length: " << header.content_length << '\n'
+                    << "[VERBOSE]      padding_length: " << header.padding_length << '\n';
+                switch (header.type) {
+                    case FCGIHeaderType::BEGIN_REQUEST:
+                        {
+                            std::cerr << "[VERBOSE] fcgi begin request\n";
+                            // doing nothing for now
+                        } break;
+                    case FCGIHeaderType::END_REQUEST:
+                        {
+                            std::cerr << "[VERBOSE] fcgi end request\n";
+                            // doing nothing for now
+                        } break;
+                    case FCGIHeaderType::FCGI_PARAMS:
+                        {
+                            std::cerr << "[VERBOSE] fcgi params\n";
+                            size_t param_buffer_index = 0;
+                            while (param_buffer_index < header.content_length) {
+                                std::cerr << "[VERBOSE] param buffer index = " << param_buffer_index << '\n';
+                                int name_length = static_cast<int>(static_cast<unsigned char>(recv_buffer[buffer_index + param_buffer_index + 0]));
+                                int value_length = static_cast<int>(static_cast<unsigned char>(recv_buffer[buffer_index + param_buffer_index + 1]));
+                                assert(name_length >> 7 == 0);
+                                assert(value_length >> 7 == 0);
+                                std::string name(&recv_buffer[buffer_index + param_buffer_index + 2], name_length);
+                                std::string value(&recv_buffer[buffer_index + param_buffer_index + name_length + 2], value_length);
+                                std::cerr << "[VERBOSE] param name: " << name << " - param value: " << value << '\n';
+                                params.emplace(name, value);
+                                param_buffer_index += 2 + name_length + value_length;
+                            }
+                        } break;
+                    case FCGIHeaderType::FCGI_STDIN:
+                        {
+                            std::cerr << "[VERBOSE] fcgi stdin\n";
+                            fcgi_stdin += std::string(&recv_buffer[buffer_index], header.content_length);
+                            std::cerr << "[VERBOSE] stdin: " << fcgi_stdin << '\n';
+                        } break;
+                    case FCGIHeaderType::FCGI_STDOUT:
+                        {
+                            std::cerr << "[VERBOSE] fcgi stdout\n";
+                        } break;
+                    case FCGIHeaderType::FCGI_STDERR:
+                        {
+                            std::cerr << "[VERBOSE] fcgi stderr\n";
+                        } break;
+                    default:
+                        assert(false);
+                }
+                buffer_index += header.content_length + header.padding_length;
             }
+
+            std::string fcgi_stdout = handle_request(std::move(fcgi_stdin), params);
+
+            std::vector<char> response;
+            response.push_back(static_cast<char>(1));
+            response.push_back(static_cast<char>(FCGIHeaderType::FCGI_STDOUT));
+            response.push_back(static_cast<char>(request_id / 256));
+            response.push_back(static_cast<char>(request_id % 256));
+            response.push_back(static_cast<char>(fcgi_stdout.size() / 256));
+            response.push_back(static_cast<char>(fcgi_stdout.size() % 256));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            for (char c: fcgi_stdout) {
+                response.push_back(c);
+            }
+
+            int bytes_sent = send(fd, response.data(), response.size(), 0);
+            if (bytes_sent != response.size()) {
+                std::cerr << "[ERROR] not enough bytes sent\n";
+            } else {
+                std::cerr << "[VERBOSE] " << bytes_sent << " bytes sent\n";
+            }
+            response.clear();
+
+            response.push_back(static_cast<char>(1));
+            response.push_back(static_cast<char>(FCGIHeaderType::FCGI_STDERR));
+            response.push_back(static_cast<char>(request_id / 256));
+            response.push_back(static_cast<char>(request_id % 256));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+
+            bytes_sent = send(fd, response.data(), response.size(), 0);
+            if (bytes_sent != response.size()) {
+                std::cerr << "[ERROR] not enough bytes sent\n";
+            } else {
+                std::cerr << "[VERBOSE] " << bytes_sent << " bytes sent\n";
+            }
+            response.clear();
+
+            response.push_back(static_cast<char>(1));
+            response.push_back(static_cast<char>(FCGIHeaderType::END_REQUEST));
+            response.push_back(static_cast<char>(request_id / 256));
+            response.push_back(static_cast<char>(request_id % 256));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(8));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+            response.push_back(static_cast<char>(0));
+
+            //std::cerr << "[VERBOSE] response: ";
+            //for (char byte: response) {
+            //    std::cerr << std::hex << byte;
+            //}
+            //std::cerr << '\n';
+
+            bytes_sent = send(fd, response.data(), response.size(), 0);
+            if (bytes_sent != response.size()) {
+                std::cerr << "[ERROR] not enough bytes sent\n";
+            } else {
+                std::cerr << "[VERBOSE] " << bytes_sent << " bytes sent\n";
+            }
+
             memset(&recv_buffer, 0, RECV_BUFFER_SIZE);
         }
     }
