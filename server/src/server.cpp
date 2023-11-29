0e65fc5cea82b4ed63bc9b19368f8266602469a3
rivten <rivten@riseup.net>
2023-11-29 22:16:17 +0100

    fastcgi uses status

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -5,7 +5,7 @@
 std::string handle_request(std::string fcgi_stdin, const std::unordered_map<std::string, std::string>& params) {
     std::string http_content = fcgi_stdin;
     std::string fcgi_stdout;
-    fcgi_stdout += "HTTP/1.1 201 Created\r\n";
+    fcgi_stdout += "Status: 200\r\n";
     fcgi_stdout += "Content-Length: ";
     fcgi_stdout += std::to_string(http_content.length());
     fcgi_stdout += "\r\n";
